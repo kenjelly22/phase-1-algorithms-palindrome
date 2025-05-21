c284ce01853a9eeff5f042c8221e778a7a465f4e
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-21 09:52:59 -0400

    Re-join reversed letters

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function isPalindrome(word) {
   // Write your algorithm here
-  const reversed = word.split("").reverse("")
+  const reversed = word.split("").reverse("").join("")
   return reversed
 }
 
