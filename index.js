d263631d45cf85f9132f6d4ce845559bf4c815d6
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-21 09:51:11 -0400

    Return word split into an array of it's letters

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversed = word.split("")
+  return reversed
 }
 
 /* 
   Add your pseudocode here
+  - Input: String
+    - Function takes string as argument.
+
+  - Determine if string is a palindrome.
+    - .split string into individual letters and return an array
+    - reverse array
+    - re-join the individual letters to get the original work in reverse
+    - Use if/else to compare original word and reversed word
+
+  - Output: Boolean
+    - Returns True if String is a palindrome
+      - if original word === reversed word = true
+    - Returns False if String is not a Palindrome
+      - if original word !== reversed word = false
 */
 
 /*
@@ -13,13 +29,15 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log(isPalindrome("rain"))
 
-  console.log("");
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("racecar"))
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("")
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("robot"))
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
